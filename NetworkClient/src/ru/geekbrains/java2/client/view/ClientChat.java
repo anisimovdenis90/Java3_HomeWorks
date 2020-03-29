5315a1106a78ef73e9de284645a091242b45f5f8
Denis Anisimov <anisimov.denis.90@gmail.com>
2020-03-29 19:03:41 +0300

    Добавил сетевой чат.

---
+++ NetworkClient/src/ru/geekbrains/java2/client/view/ClientChat.java
@@ -0,0 +1,101 @@
+package ru.geekbrains.java2.client.view;
+
+import ru.geekbrains.java2.client.controller.ClientController;
+
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.List;
+
+public class ClientChat extends JFrame {
+
+    private JPanel mainPanel;
+    private JList<String> usersList;
+    private JTextField messageTextField;
+    private JButton sendButton;
+    private JTextArea chatText;
+    // Ссылка на контроллер
+    private ClientController controller;
+    private String selectedContact = "All";
+
+    public ClientChat(ClientController controller) {
+        this.controller = controller;
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(640, 480);
+        setLocationRelativeTo(null);
+        setContentPane(mainPanel);
+        addListeners();
+        // При закрытии окна отключаем клиента от сервера
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                controller.sendEndMessage();
+                controller.shutdown();
+            }
+        });
+    }
+
+    /**
+     * Добавление действий на кнопку и поле ввода текста
+     */
+    private void addListeners() {
+        sendButton.addActionListener(e -> ClientChat.this.sendMessage());
+        messageTextField.addActionListener(e -> sendMessage());
+    }
+
+    /**
+     * Метод отправки сообщения из окна чата
+     */
+    private void sendMessage() {
+        String message = messageTextField.getText().trim();
+        if (message.isEmpty()) {
+            return;
+        }
+        // Отправка сообщения всем
+        if (usersList.getSelectedIndex() < 1) {
+            appendMessage("Я: " + message);
+            controller.sendMessageToAllUsers(message);
+        }
+        // Отправка сообщения выбранному контакту
+        else {
+            String username = usersList.getSelectedValue();
+            appendMessage("Я лично username: " + message);
+            controller.sendPrivateMessage(username, message, controller.getUsername());
+        }
+        // Очищаем окно ввода текста после отправки сообщения
+        messageTextField.setText(null);
+    }
+
+    /**
+     * Метод обновления окна чата в отдельном потоке
+     *
+     * @param message - текст полученного сообщения
+     */
+    public void appendMessage(String message) {
+        SwingUtilities.invokeLater(() -> {
+            chatText.append(message);
+            chatText.append(System.lineSeparator());
+        });
+    }
+
+    /**
+     * Отображает предупреждающее окно
+     *
+     * @param message - описание ошибки
+     */
+    public void showError(String message) {
+        JOptionPane.showMessageDialog(this, message, "Ошибка!", JOptionPane.ERROR_MESSAGE);
+    }
+
+    /**
+     * Обновление списка контактов
+     *
+     * @param users - на вход коллекция контактов
+     */
+    public void updateUsers(List<String> users) {
+        SwingUtilities.invokeLater(() -> {
+            DefaultListModel<String> model = new DefaultListModel<>();
+            model.addAll(users);
+            usersList.setModel(model);
+        });
+    }
+}
---
+++ NetworkClientServer/src/ru/geekbrains/java2/client/CommandType.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.java2.client;
+
+public enum CommandType {
+
+    AUTH,
+    AUTH_ERROR,
+    TIMEOUT_AUTH_ERROR,
+    PRIVATE_MESSAGE,
+    BROADCAST_MESSAGE,
+    MESSAGE,
+    TIMEOUT_MESSAGE,
+    UPDATE_USERS_LIST,
+    ERROR,
+    END
+
+}
